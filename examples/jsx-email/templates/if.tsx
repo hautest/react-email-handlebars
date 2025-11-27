4a67d16b4106153509e75620262d9be2736c1c2d
hautest <dos07008@naver.com>
2025-11-28 00:34:46 +0900

    feat: add jsx email example

---
+++ examples/jsx-email/templates/if.tsx
@@ -0,0 +1,22 @@
+import { Body, Head, Html, Preview, Text } from "jsx-email";
+import { If, RuntimeProvider } from "react-email-handlebars";
+
+export const templateName = "if";
+
+export const Template = () => (
+  <RuntimeProvider value="preview">
+    <Html>
+      <Head />
+      <Preview>if</Preview>
+      <Body>
+        <If
+          conditionPath="if-key"
+          previewCondition={false}
+          then={<Text style={{ color: "red" }}>You are an admin</Text>}
+          else={<Text style={{ color: "blue" }}>You are not an admin</Text>}
+        />
+        <Text>test</Text>
+      </Body>
+    </Html>
+  </RuntimeProvider>
+);
