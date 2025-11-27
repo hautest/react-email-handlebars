57196254152cab30c28a77e4f2728fd6dffd46b8
hautest <dos07008@naver.com>
2025-11-28 00:34:31 +0900

    feat: add react-email example

---
+++ examples/react-email/emails/if.tsx
@@ -0,0 +1,19 @@
+import { Body, Html, Text } from "@react-email/components";
+import { If, RuntimeProvider } from "react-email-handlebars";
+
+export default function IfExample() {
+  return (
+    <RuntimeProvider value="build">
+      <Html>
+        <Body>
+          <If
+            conditionPath="if-key"
+            previewCondition={false}
+            then={<Text>You are an admin</Text>}
+            else={<Text>You are not an admin</Text>}
+          />
+        </Body>
+      </Html>
+    </RuntimeProvider>
+  );
+}
