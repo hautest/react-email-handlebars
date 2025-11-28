900da49ff782a95a078ef7958c17b4327b9c81dc
hautest <dos07008@naver.com>
2025-11-29 00:37:07 +0900

    chore: update package.json and pnpm-lock.yaml to include @types/node and adjust peer dependencies for jsx-email

--- examples/react-email/emails/if.tsx
+++ examples/react-email/emails/if.tsx
@@ -3,12 +3,12 @@ import { If, RuntimeProvider } from "react-email-handlebars";
 
 export default function IfExample() {
   return (
-    <RuntimeProvider value="build">
+    <RuntimeProvider value="preview">
       <Html>
         <Body>
           <If
             conditionPath="if-key"
-            previewCondition={false}
+            previewCondition={true}
             then={<Text>You are an admin</Text>}
             else={<Text>You are not an admin</Text>}
           />
